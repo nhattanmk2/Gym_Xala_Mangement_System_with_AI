e6400c6fcabcc8cab85b4647783fcd65cd42ab1e
Le Ngoc Nhat Tan <101038380+nhattanmk2@users.noreply.github.com>
2026-01-27 10:22:42 +0700

    Add JWT-based Spring Security configuration

---
+++ gym-backend/src/main/java/com/xala/gym/security/UserDetailsServiceImpl.java
@@ -0,0 +1,40 @@
+package com.xala.gym.security;
+
+import com.xala.gym.entity.User;
+import com.xala.gym.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found"));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                user.getRoles().stream()
+                        .map(role ->
+                                new SimpleGrantedAuthority(
+                                        "ROLE_" + role.getName().name()
+                                )
+                        )
+                        .collect(Collectors.toSet())
+        );
+    }
+}
